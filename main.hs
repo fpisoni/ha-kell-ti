e448ca137c15a0b68892a99f5ca5935d1796f74d
Felipe Pisoni <feli.pisoni@gmail.com>
2019-10-18 00:18:49 -0300

    added functions

--- main.hs
+++ main.hs
@@ -1,5 +1,20 @@
-Type Pos = int
+---data definitions
 data Fig = Circle | Cross | Null
 data Cell = Fig
-data Row = Array Pos Cell
+data Row = Array Cell
 data Board = [Row]
+---end
+
+---0lvl funcs
+---la idea era que tome el valor literal Char x, o y n (de null)
+makeFig :: Char -> Fig
+makeFig 'x' = Cross
+makeFig 'o' = Circle
+makeFig 'n' = Null
+
+---
+---makeCell :: a -> Cell
+---makeCell = makeFig n
+
+---makeRow :: a -> Row
+---makeRow = 
