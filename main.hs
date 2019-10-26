ef1eed1cc093c69331f6191cc0dd9ca8a15cef31
Lautaro <lautaro.ml@gmail.com>
2019-10-25 21:22:02 -0300

    Agregada la funcion printFig, corregidos errores de sintaxis y agregado el archivo "Estados posibles del Board.txt" (NOT finished)

--- main.hs
+++ main.hs
@@ -1,21 +1,27 @@
----data definitions
-data Fig = Circle | Cross | Null
-data Cell = MkCell Fig --- Recorda que un tipo necesita tener un constructor si o si. Tal vez en vez de data habría que usar type
-data Row = Array Cell --- Una fila es de 3*1, con lo que no deberia ser una lista de Cell? Si querés usar array creo que Board tendria que ser un Array de Cell
-data Board = [Row]
----end
+-- data definitions
+data Fig   = Circle | Cross | Null
+data Cell  = MkCell Fig    -- Recorda que un tipo necesita tener un constructor si o si. Tal vez en vez de data habría que usar type
+data Row   = MkRow [Cell]
+data Board = Mkboard [Row] -- Board deberia ser una matriz de Cell
+-- end
 
----0lvl funcs
----la idea era que tome el valor literal Char x, o y n (de null)
+-- 0lvl funcs
+-- la idea era que tome el valor literal Char x, o y n (de null)
 
 makeFig :: Char -> Fig
 makeFig 'x' = Cross
 makeFig 'o' = Circle
 makeFig 'n' = Null
---- Creo que las input se hacen de otra forma, hay que chekear la monada IO y ver como se piden inputs del usuario
+-- Creo que las input se hacen de otra forma, hay que chekear la monada IO y ver como se piden inputs del usuario
 
-makeCell :: a -> Cell
-makeCell = makeFig n
+printFig :: Fig -> Char
+printFig Cross  = 'x'
+printFig Circle = 'o'
+printFig Null   = '-'
+-- Creo que vamos a necesitar esto para imprimir el Board
 
-makeRow :: a -> Row
-makeRow =
+-- makeCell :: a -> Cell
+-- makeCell = makeFig n
+
+-- makeRow :: a -> Row
+-- makeRow =
