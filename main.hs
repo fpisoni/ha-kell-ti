47cda4a7bfac72aefb5c13fc31d9b4d113d84e70
Lautaro <lautaro.ml@gmail.com>
2019-11-28 12:54:18 -0300

    Agregado el uso de Arrays para el Board y la lectura del archivo Board.txt (eso ultimo puede estar mal)

--- main.hs
+++ main.hs
@@ -1,17 +1,32 @@
+import Data.Array.Repa
+
 -- data definitions
 data Fig   = Circle | Cross | Null
 data Cell  = MkCell Fig    -- Recorda que un tipo necesita tener un constructor si o si. Tal vez en vez de data habría que usar type
-data Row   = MkRow [Cell]
-data Board = Mkboard [Row] -- Board deberia ser una matriz de Cell
+data Board = MkBoard (Array U DIM2 Cell)
+-- data Row   = MkRow [Cell]
+-- data Board = Mkboard [Row] -- Board deberia ser una matriz de Cell
 -- end
+
+main = do
+          contents  <- readFile "C:\\Users\\Lautarito\\Desktop\\has-kell-ti\\board"
+          let cells = map readFig (takeWhile (/= ' ') contentents)
+          let board = fromListUnboxed (Z :. (3::int) :. (3::int)) cells
+          putStr "not finished" -- Es necesario el putStr para que no se rompa la monada
 
 -- 0lvl funcs
 -- la idea era que tome el valor literal Char x, o y n (de null)
+
+readFig :: Char -> Cell
+readFig 'x' = MkCell Cross
+readFig 'o' = MkCell Circle
+readFig '-' = MkCell Null
 
 makeFig :: Char -> Fig
 makeFig 'x' = Cross
 makeFig 'o' = Circle
 makeFig 'n' = Null
+-- Cada vez que veo esto me convenzo mas de que es inecesaria esta funcion
 -- Creo que las input se hacen de otra forma, hay que chekear la monada IO y ver como se piden inputs del usuario
 
 printFig :: Fig -> Char
